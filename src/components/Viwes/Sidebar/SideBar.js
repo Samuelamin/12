d56fbf1605fa6c478a3df7dcfe4b7353d51e0560
samuelamin <samuelamin1999@gmail.com>
2021-12-12 19:45:59 +0200

    changes

--- src/components/Viwes/Sidebar/SideBar.js
+++ src/components/Viwes/Sidebar/SideBar.js
@@ -1,50 +1,49 @@
-import React  from 'react';
+import React ,{useState} from 'react';
 import "./SideBar.css"
 import { ProductConsumer } from '../../context';
 import { Link } from 'react-router-dom';
 
-const SideBar = () => {
+export default function SideBar() {
+  const [hide , setHide] = useState(false);
   return (
-      <nav className='sidebar' >
-        <h4>BABA  </h4>
-       
-      <ul className='nav-menu-items' style={{paddingLeft:"initial"}}>
+    <nav className='sidebar' onMouseEnter={()=>setHide(true)} onMouseLeave={()=>setHide(false)} >
+    <h4>BABA  </h4>
+   
+  <ul className='nav-menu-items' style={{paddingLeft:"initial"}}>
 
-      <ProductConsumer>
-            {value =>{
-                return value.courses.map(course =>{
-                  const {title ,icon , id } =course;
-                    return (
-                      <ProductConsumer key={id}>
-                      {value => (
-                                 <div className='side-menu'  >
-                                 <li className='nav-text'  >
-                                     <Link to='/CursePage'  onClick={ ()=> value.handlecourseDetail(id)} >
-                                        {icon} <span>{title}</span> 
-                                     </Link>
-                                 </li>
-                     
-                             </div>
-                      )}
-            </ProductConsumer>
+  <ProductConsumer>
+        {value =>{
+            return value.courses.map(course =>{
+              const {title ,icon , id } =course;
+                return (
+                  <ProductConsumer key={id}>
+                  {value => (
+                             <div className='side-menu'  >
+                             <li className='nav-text'  >
+                                 <Link to='/CursePage'  onClick={ ()=> value.handlecourseDetail(id)} >
+                                    {icon} {hide? <span>{title}</span> : null}
+                                 </Link>
+                             </li>
+                 
+                         </div>
+                  )}
+        </ProductConsumer>
 
-                    )
-                })
-            }}
-            </ProductConsumer>
+                )
+            })
+        }}
+        </ProductConsumer>
 
-      </ul>
-      <div >  
-          <div className='support'>
-          <i class="fas fa-info-circle"></i> <span>Support</span>
-          </div>
-          <div className='support'>
-          <i class="fas fa-user-friends"></i> <span> Comapny Details </span> 
-          </div>
+  </ul>
+  <div >  
+    
+      <div className='support'>
+      <i class="fas fa-info-circle"></i> {hide? <span>Support</span> :null}
       </div>
-      </nav>  
-      
+      <div className='support'>
+      <i class="fas fa-user-friends"></i> {hide? <span> Comapny Details </span> : null} 
+      </div>
+  </div>
+  </nav>  
   )
 }
-
-export default SideBar
